eca6f16617651127525ae457133fd32a076df342
+ <+>
2021-08-30 16:41:14 -0700

    Advanced functions lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return [drivers[0], drivers[1]];
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    return [drivers[drivers.length - 2], drivers[drivers.length - 1]];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare) {
+    return function(miles) {
+        return miles * fare;
+    };
+}
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(fare)(1) * 2;
+};
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(fare)(1) * 3;
+};
+
+function selectDifferentDrivers(drivers, functionArg) {
+    return functionArg(drivers);
+}
